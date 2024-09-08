292493772151facf41878627bafc034e01b52104
dianlujitao <dianlujitao@lineageos.org>
2024-09-08 21:26:39 +0800

    sdm845-common: Remove OMX
     * OMX HW video codecs are no longer used after enabling Codec2
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
    
    Change-Id: I65852151c8fb6387add5fc4402ba3f13c97b5f64

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -434,8 +434,6 @@ PRODUCT_PACKAGES += \
     com.qti.chi.override \
     sound_trigger.primary.sdm845 \
     libFileMux_proprietary \
-    libOmxVideoDSMode \
-    libOmxVpp \
     libSTDualCamCaptureBokeh \
     libSTDualCamPreviewBokeh \
     libSTdisp \
