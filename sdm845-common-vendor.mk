b38f857272a2fdd912af8201732c7527f28881c6
Terminator-J <terminator_j@hotbutteredmonkey.com>
2025-03-15 20:57:28 -0700

    sdm845-common: camera: Add halide/hexagon blobs
    Needed for OnePlus Camera support
    
    Updated for Android 15.0:
    - Strip symbol versions & enable elf checks

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -479,6 +479,7 @@ PRODUCT_PACKAGES += \
     libgcs-ipc \
     libgcs-osal \
     libgcs \
+    libhalide_hexagon_host \
     liblistensoundmodel2 \
     libmm-hdcpmgr \
     libmmcamera_faceproc \
